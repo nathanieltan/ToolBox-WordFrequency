2eacff7b4a6c15f8609046da2a64998460fc5806
nathanieltan <nathaniel98.tan@gmail.com>
2017-03-10 13:11:36 -0500

    Finished toolbox

--- frequency.py
+++ frequency.py
@@ -4,14 +4,29 @@ Project Gutenberg """
 import string
 
 
-def get_word_list(file_name):
+def get_word_list(file_name="pg1260.txt"):
     """ Reads the specified project Gutenberg book.  Header comments,
     punctuation, and whitespace are stripped away.  The function
     returns a list of the words used in the book as a list.
     All words are converted to lower case.
     """
-    pass
-
+    words = []
+    f = open(file_name, 'r')
+    lines = f.readlines()
+    start_line = 0
+    end_line = 0
+    while lines[start_line].find('START OF THE PROJECT GUTENBERG EBOOK') == -1:
+        start_line += 1
+    while lines[end_line].find('***END OF THE PROJECT GUTENBERG EBOOK JANE EYRE***') == -1:
+        end_line+=1
+    lines = lines[start_line+1:end_line]
+    trantab = str.maketrans('','',string.punctuation)
+    for x in range(len(lines)):
+        line = lines[x].strip().split()
+        for a in range(len(line)):
+            word = line[a].lower()
+            words.append(word.translate(trantab))
+    return words
 
 def get_top_n_words(word_list, n):
     """ Takes a list of words as input and returns a list of the n most frequently
@@ -23,8 +38,17 @@ def get_top_n_words(word_list, n):
     returns: a list of n most frequently occurring words ordered from most
     frequently to least frequentlyoccurring
     """
-    pass
+    frequency = {}
+    result = []
+    for x in range(len(word_list)):
+        frequency[word_list[x]] = frequency.get(word_list[x],0)+1
+    sortedFrequency = sorted(frequency, key=frequency.__getitem__, reverse = True)
+    counter = 0
+    for key in sortedFrequency:
+        result.append(key)
+        counter += 1
+    return result[0:n]
 
 if __name__ == "__main__":
     print("Running WordFrequency Toolbox")
-    print(string.punctuation)
+    print(get_top_n_words(get_word_list(),100))
